4dcd63c96233905623679993d3e5225662907883
kiwon <kkw-11@naver.com>
2025-06-10 19:26:11 +0900

    test:JPA setting test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,32 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         //code
+        try{
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
 
-        em.close();
+            for (Member member : result) {
+                System.out.println("member.name: " + member.getName());
+            }
+
+            tx.commit();
+
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
