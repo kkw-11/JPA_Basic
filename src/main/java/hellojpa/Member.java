836f9beef88b0fdf29b3b58c673a1926b80bed54
kiwon <kkw-11@naver.com>
2025-06-12 13:07:32 +0900

    feat:Member 모든 필드 생성자

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,7 +12,10 @@ public class Member {
     public Member() {
     }
 
-    public Member(Long id, String name) {}
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
